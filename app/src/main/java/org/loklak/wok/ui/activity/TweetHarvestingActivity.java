6d7cffa5b6784619dfa45af4a5bccd39f093c77c
arundhati24 <arundhatigupta2411@gmail.com>
2018-01-26 19:29:05 +0530

    Add 'Press again to exit' (#110)
    Prompt the user to hit the back button twice to exit from the app.

--- app/src/main/java/org/loklak/wok/ui/activity/TweetHarvestingActivity.java
+++ app/src/main/java/org/loklak/wok/ui/activity/TweetHarvestingActivity.java
@@ -1,14 +1,18 @@
 package org.loklak.wok.ui.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import org.loklak.wok.ui.fragment.TweetHarvestingFragment;
 import org.loklak.wok.R;
 
 public class TweetHarvestingActivity extends AppCompatActivity {
+
+    private boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,5 +26,23 @@ public class TweetHarvestingActivity extends AppCompatActivity {
                     .replace(R.id.fragment_container, tweetHarvestingFragment)
                     .commit();
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Press again to exit", Toast.LENGTH_SHORT)
+                .show();
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
     }
 }
