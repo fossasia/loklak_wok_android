a22f75c76f2457aae5eba32f1c1b95d77f458be6
Riya Singh <43132209+riyasingh1004@users.noreply.github.com>
2019-02-05 10:40:54 +0530

    Double back press to exit the app #110 (#125)
    *  Double back press to exit the app #110
    
    The user needs to press the back button twice to exit the app.
    
    * Press back button twice to exit #110
    
    Adding some changes to the previous commit
    
    * Updating #110
    
    Declaring the import files.

--- app/src/main/java/org/loklak/wok/ui/activity/TweetHarvestingActivity.java
+++ app/src/main/java/org/loklak/wok/ui/activity/TweetHarvestingActivity.java
@@ -1,7 +1,6 @@
 package org.loklak.wok.ui.activity;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
@@ -12,7 +11,7 @@ import org.loklak.wok.R;
 
 public class TweetHarvestingActivity extends AppCompatActivity {
 
-    private boolean doubleBackToExitPressedOnce = false;
+    private static long BackPressed;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,19 +29,13 @@ public class TweetHarvestingActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (doubleBackToExitPressedOnce) {
+        if (BackPressed+2000>System.currentTimeMillis()) {
+            
             super.onBackPressed();
-            return;
         }
-        this.doubleBackToExitPressedOnce = true;
-        Toast.makeText(this, "Press again to exit", Toast.LENGTH_SHORT)
-                .show();
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                doubleBackToExitPressedOnce = false;
-            }
-        }, 2000);
+        else{
+            Toast.makeText(getBaseContext(),"Press once again to exit",Toast.LENGTH_SHORT).show();
+        }
+        BackPressed = System.currentTimeMillis();
     }
 }
